547384eec4ff1f6ae0b7dcfc31c83984928a1e97
Zach Parliman <zparliman@crossroadsbiblechurch.com>
2024-11-08 14:06:11 -0700

    Add Sermon model and upload view

--- sermon_express/settings.py
+++ sermon_express/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # User apps
+    'upload.apps.UploadConfig',
+    # Default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- upload/models.py
+++ upload/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Sermon(models.Model):
+    title = models.CharField(max_length=255)
+    date = models.DateField()
+    scripture = models.CharField(max_length=255)
+    series = models.CharField(max_length=255)
+    topic = models.CharField(max_length=255)
+    speaker = models.CharField(max_length=255)
+    audio = models.FileField()
+    slides = models.FileField()
